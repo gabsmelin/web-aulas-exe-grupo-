7e72558c7bfa2aa27afd54390b0d1213c94ab8d3
gabsmelin <rm550202@fiap.com.br>
2023-09-15 17:57:02 -0300

    html da página feito

--- projetinho02/src/routes/AdicionarProduto.jsx
+++ projetinho02/src/routes/AdicionarProduto.jsx
@@ -1,7 +1,31 @@
 
 export default function AdicionarProduto() {
+    document.title = "Adicionar Produtos";
+    
     return(
         <div>
+            <h1>Adicionar Produto</h1>
+            <div>
+                <form action="">
+                    <fieldset>
+                        <legend>Adicionar Produto</legend>
+                        <div>
+                            <input type="hidden" id="id" />
+                        </div>
+                        <div>
+                            <label htmlFor="idNome">Nome:</label>
+                            <input type="text" name="nome" id="idNome"/>
+                        </div>
+                        <div>
+                            <label htmlFor="idPreco">Preço:</label>
+                            <input type="number" name="preco" id="idPreco"/>
+                        </div>
+                        <div>
+                            <button onClick={handleAdd}>Adicionar</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     )
 }
