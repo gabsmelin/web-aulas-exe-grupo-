266167581e89828a82cf655ede27c528898002da
gabsmelin <rm550202@fiap.com.br>
2023-09-15 17:59:41 -0300

    função handleChange criada para atualizar valor do input até chegar no result final

--- projetinho02/src/routes/AdicionarProduto.jsx
+++ projetinho02/src/routes/AdicionarProduto.jsx
@@ -13,7 +13,15 @@ export default function AdicionarProduto() {
         nome: '',
         preco: '',
     });
-    
+
+    //Atulizando escrita dentro do input até chegar no resultado final(escrita que irá ser adicionada na nossa lista)
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+
+        setProduto({...produtos,[name]:value})
+        console.log(produtos.nome)
+    }
+
     return(
         <div>
             <h1>Adicionar Produto</h1>
@@ -26,11 +34,11 @@ export default function AdicionarProduto() {
                         </div>
                         <div>
                             <label htmlFor="idNome">Nome:</label>
-                            <input type="text" name="nome" id="idNome"/>
+                            <input type="text" name="nome" id="idNome" value={produtos.nome}/>
                         </div>
                         <div>
                             <label htmlFor="idPreco">Preço:</label>
-                            <input type="number" name="preco" id="idPreco"/>
+                            <input type="number" name="preco" id="idPreco" value={produtos.preco}/>
                         </div>
                         <div>
                             <button onClick={handleAdd}>Adicionar</button>
